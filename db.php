9f73844b78010b34d613e9926df85c3564920992
Aphly <121099327@qq.com>
2022-11-04 11:40:00 +0800

    update

--- db.php
+++ db.php
@@ -15,7 +15,29 @@ if($dbName){
 //$db = new PDO($dsn, $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 
 //UPDATE product LEFT JOIN product_description ON product.product_id=product_description.product_id SET product.`status`=0 WHERE product_description.name LIKE '%Thermometer%';
-$arr = [];
+
+//$arr['chicire_com'] = ['db'=>['christiy','sudarin']];
+//$arr['corekoe_com'] = ['db'=>['daphare','chrinea_com','harinch_com','patelsi_com','miccain_com','boriden_com','raquely_com','helenue_com']];
+//$arr['findunds_com'] = ['db'=>['kimiber','aunites_com','hisomiy_com','niarsha_com','cynilon_com','bridany_com','yobouet_com']];
+//$arr['ithrzn_com'] = ['db'=>['lindaey','dareney_com','himenit_com','lutoya_com','vishate_com','yaledia_com','naclari_com','karsony_com']];
+//$arr['knittes_com'] = ['db'=>['lizeben']];
+//$arr['pintzxcr_com'] = ['db'=>['magenri','canites_com','milisar_com','shanany_com','tamarse_com','karenty_com','paricen_com']];
+//$arr['siniand_com'] = ['db'=>['pameniy','burkera_com','grovicn_com','resaniy_com','buridal_com','sariaen_com']];
+//$arr['toskaha_com'] = ['db'=>['ravaley','lourder_com','mathria_com','wilsion_com','caspern_com','arsilin_com','tareken_com','casperneu_com']];
+//$arr['visiive_com'] = ['db'=>['ricadom']];
+
+$arr['corekoe_com'] = ['db'=>['patelsi_com','boriden_com','raquely_com','helenue_com']];
+$arr['findunds_com'] = ['db'=>['niarsha_com','cynilon_com','bridany_com','yobouet_com','mathria_com','wilsion_com','arsilin_com','tareken_com']];
+$arr['ithrzn_com'] = ['db'=>['lutoya_com','vishate_com','yaledia_com','naclari_com','karsony_com']];
+$arr['pintzxcr_com'] = ['db'=>['milisar_com','shanany_com','tamarse_com','karenty_com','paricen_com']];
+$arr['siniand_com'] = ['db'=>['resaniy_com','buridal_com','sariaen_com']];
+
+$arr['findunds_com']['product_ids'] = [3198,3172,3188,3200,3184,3190,3178,3187,3196,3189,3007,3034,3003,3004,3026,3019,3065,3001,3068,3054,3207];
+//$arr['toskaha_com']['product_ids'] = [214,62];
+$arr['corekoe_com']['product_ids'] = [49];
+$arr['pintzxcr_com']['product_ids'] = [7391,7385];
+
+
 try {
     $dbh = new PDO($dsn, $user, $pass);
     echo "连接成功<br/>";
